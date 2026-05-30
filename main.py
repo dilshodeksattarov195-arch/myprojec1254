6060841b8b35a82316611e3082626d817814fb5d
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 21:13:43 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class notifyUenderWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = notifyUenderWorker()
+    print("Worker engine initialized.")
